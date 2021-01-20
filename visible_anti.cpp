a389cfccf0aa1bc4a1d901fd191284dd76272891
Dipyaman Pramanik <77561868+dpramanik92@users.noreply.github.com>
2021-01-19 21:56:50 -0300

    create visible anti cpp
    these will contain the code of visible decay for anti-neutrino appearance probability

---
+++ visible_anti.cpp
@@ -0,0 +1,11 @@
+//
+//  visiible_anti.cpp
+//  
+//
+//  Created by Dipyaman Pramanik on 19/01/21.
+//
+
+#include "visiible_anti.hpp"
+
+#include <fstream>
+#include<cmath>
---
+++ visible_anti.hpp
@@ -0,0 +1,34 @@
+//
+//  visiible_anti.hpp
+//  
+//
+//  Created by Dipyaman Pramanik on 19/01/21.
+//
+
+#ifndef visiible_anti_hpp
+#define visiible_anti_hpp
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <functional>
+#include "read_files.hpp"
+#include "numerical.hpp"
+#include "interactions.hpp"
+
+
+
+class visiible_anti_prob
+{
+private:
+    
+public:
+    
+    
+    
+}
+
+#endif /* visiible_anti_hpp */
